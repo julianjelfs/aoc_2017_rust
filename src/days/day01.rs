4b4be1c39b437a9697a9d21c5733903d2460b890
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-12 09:53:36 +0100

    golfing

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -3,33 +3,19 @@ pub fn run(input: &str) {
     println!("Day 01, Part 2: {}", part2(input));
 }
 
-fn next_index(i: usize, l: usize) -> usize {
-   (i + 1) % l
-}
-
-fn halfway(i: usize, l: usize) -> usize {
-    (i + l / 2) % l
-}
-
-fn solve(input: &str, index_finder: fn(usize, usize) -> usize) -> u32 {
+fn solve<F>(input: &str, index_finder: F) -> u32
+where
+    F: Fn(usize, usize) -> usize
+{
     let digits: Vec<u32>  = input.chars().map(|c| c.to_digit(10).expect("not a digit")).collect();
     let len = digits.len();
-    let mut total = 0;
-
-    for i in 0..len {
-        if let (Some(a), Some(b)) = (digits.get(i), digits.get(index_finder(i, len))) {
-            if a == b {
-                total += a;
-            }
-        }
-    }
-    total
+    digits.iter().enumerate().filter(|(i, &d)| d == digits[index_finder(*i, len)]).map(|(_, d)| d).sum()
 }
 
 fn part1(input: &str) -> u32 {
-    solve(input, next_index)
+    solve(input, |i, l| (i + 1) % l)
 }
 
 fn part2(input: &str) -> u32 {
-    solve(input, halfway)
+    solve(input, |i, l| (i + l / 2) % l)
 }

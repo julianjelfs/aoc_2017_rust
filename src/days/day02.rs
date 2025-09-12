c65f65dc01f487cfd5f2f03f8df41751d7961459
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-12 17:17:07 +0100

    tidy up

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -3,20 +3,18 @@ pub fn run(input: &str) {
     println!("Day 01, Part 2: {}", part2(input));
 }
 
-fn minmax(digits: &Vec<u32>) -> u32 {
+fn minmax(digits: &[u32]) -> u32 {
     let max = digits.iter().max().unwrap();
     let min = digits.iter().min().unwrap();
     max - min
 }
 
-fn divisible(digits: &Vec<u32>) -> u32 {
+fn divisible(digits: &[u32]) -> u32 {
     let mut result = 0;
     for x1 in digits.iter() {
         for y1 in digits.iter() {
-            if x1 != y1 {
-                if x1 % y1 == 0 {
-                    result += x1 / y1;
-                }
+            if x1 != y1 && x1 % y1 == 0 {
+                result += x1 / y1;
             }
         }
     }
@@ -25,16 +23,18 @@ fn divisible(digits: &Vec<u32>) -> u32 {
 
 fn solve<F>(input: &str, logic: F) -> u32
 where
-    F: Fn(&Vec<u32>) -> u32,
+    F: Fn(&[u32]) -> u32,
 {
-    let lines: Vec<&str> = input.lines().collect();
-    let mut total = 0;
-
-    for line in lines {
-        let digits: Vec<u32> = line.split_whitespace().filter_map(|word| word.parse().ok()).collect();
-        total += logic(&digits);
-    }
-    total
+    input
+        .lines()
+        .map(|line| {
+            let digits: Vec<u32> = line
+                .split_whitespace()
+                .filter_map(|word| word.parse().ok())
+                .collect();
+            logic(&digits)
+        })
+        .sum()
 }
 
 fn part2(input: &str) -> u32 {

88da7985c07c7f4d228ca4826cd1e071a37247df
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-15 10:21:14 +0100

    day 3 part 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,11 +1,10 @@
+use std::collections::HashMap;
+
 pub fn run(input: &str) {
     println!("Day 01, Part 1: {}", part1(input));
     println!("Day 01, Part 2: {}", part2(input));
 }
 
-fn part2(_input: &str) -> u32 {
-    0
-}
 
 enum Direction {
     Up,
@@ -14,10 +13,37 @@ enum Direction {
     Right,
 }
 
+fn neighbour_coordinates((x, y): (i32, i32)) -> Vec<(i32, i32)> {
+    vec![
+        ((x + 1), y),
+        (x + 1, y - 1),
+        (x, y - 1),
+        (x - 1, y - 1),
+        (x - 1, y ),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1)
+    ]
+}
+
+fn sum_of_neighbours(coord: (i32, i32), values: &HashMap<(i32, i32), u32>) -> u32 {
+    neighbour_coordinates(coord).iter().filter_map(|c| values.get(c)).sum()
+}
+
 fn part1(input: &str) -> u32 {
+    solve(input, false)
+}
+
+fn part2(input: &str) -> u32 {
+    solve(input, true)
+}
+
+fn solve(input: &str, check_neighbours: bool) -> u32 {
     let steps: u32 = input.parse().ok().unwrap();
     let mut x: i32 = 0;
     let mut y: i32 = 0;
+    let mut values: HashMap<(i32, i32), u32> = HashMap::new();
+    values.insert((0,0), 1);
 
     let mut left = 0;
     let mut right = 0;
@@ -41,6 +67,15 @@ fn part1(input: &str) -> u32 {
             Direction::Up if y < top => { top -= 1; Direction::Left },
             _ => direction,
         };
+
+        if check_neighbours {
+            let sum = sum_of_neighbours((x, y), &values);
+            if sum > steps {
+                return sum
+            } else {
+                values.insert((x, y), sum);
+            }
+        }
     }
 
     (x.abs() + y.abs()) as u32

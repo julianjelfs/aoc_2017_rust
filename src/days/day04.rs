c6ad8fef1c1482f85e01c3467a3eae2e4d88e975
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-15 12:36:59 +0100

    day 4 part 2

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,12 +1,47 @@
+use std::{collections::HashSet };
+
+use itertools::Itertools;
+
 pub fn run(input: &str) {
-    println!("Day 01, Part 1: {}", part1(input));
-    println!("Day 01, Part 2: {}", part2(input));
+    println!("Day 04, Part 1: {}", part1(input));
+    println!("Day 04, Part 2: {}", part2(input));
 }
 
-fn part1(_input: &str) -> u32 {
-    0
+fn contains_anagrams(words: &[&str]) -> bool {
+    let mut s = HashSet::new();
+    for &word in words {
+        let sorted: String = word.chars().sorted_unstable().collect();
+        if !s.insert(sorted) {
+            return true
+        }
+    }
+    false
 }
 
-fn part2(_input: &str) -> u32 {
-    0
+fn contains_duplicates(words: &[&str]) -> bool {
+    let mut set = HashSet::new();
+    for &word in words {
+        if !set.insert(word) {
+            return true
+        }
+    }
+    false
+}
+
+fn solve<F>(input: &str, check: F) -> usize
+where
+    F: Fn(&[&str]) -> bool,
+{
+    input.lines().filter(|line| {
+        let words: Vec<&str> = line.split_whitespace().collect();
+        !check(&words)
+    }).count()
+}
+
+fn part1(input: &str) -> usize {
+    solve(input, contains_duplicates)
+}
+
+fn part2(input: &str) -> usize {
+    solve(input, contains_anagrams)
 }
